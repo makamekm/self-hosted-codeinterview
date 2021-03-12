e75e87b94194e3a6b7b05e4018a48763aca6c74c
makame <makame@mail.ru>
2021-03-12 14:00:08 +0100

    fixes

--- src/env/prod/config.ts
+++ src/env/prod/config.ts
@@ -5,10 +5,9 @@ export const CORS = true;
 export const WEB_SERVER_PORT = process.env.PORT || 8080;
 export const WEB_SERVER_HOST = process.env.HOST || "0.0.0.0";
 export const PUBLIC_FOLDER = path.resolve("./public");
-export const SOCKET_SERVER = "https://makame.rowanberry.xyz";
-export const URI = "https://makame.rowanberry.xyz";
-export const GOOGLE_AUTH_REDIRRECT =
-  "https://makame.rowanberry.xyz/api/google/redirect";
+export const SOCKET_SERVER = process.env.SELF_URL;
+export const URI = process.env.SELF_URL;
+export const GOOGLE_AUTH_REDIRRECT = `${process.env.SELF_URL}/api/google/redirect`;
 export const MONGO_CONNECTION_URL = process.env.MONGO_CONNECTION_URL;
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 export const GOOGLE_SECRET = process.env.GOOGLE_SECRET;
