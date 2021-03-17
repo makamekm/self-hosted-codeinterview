46d80a0ca1b9d7299dacc6ed9539df92c78f10bf
makamekm <makame@mail.ru>
2021-03-17 11:43:39 +0100

    fixes for ws cluster

--- src/adapters/socket.redis.adapter.ts
+++ src/adapters/socket.redis.adapter.ts
@@ -1,6 +1,7 @@
 import { IoAdapter } from "@nestjs/platform-socket.io";
 import { RedisClient } from "redis";
 import { createAdapter } from "socket.io-redis";
+import { setupWorker } from "@socket.io/sticky";
 import { REDIS_CONFIG } from "@env/config";
 
 export class SocketRedisAdapter extends IoAdapter {
@@ -10,6 +11,7 @@ export class SocketRedisAdapter extends IoAdapter {
     const subClient = pubClient.duplicate();
     const redisAdapter = createAdapter({ pubClient, subClient });
     server.adapter(redisAdapter);
+    setupWorker(server);
     return server;
   }
 }
--- src/providers/cluster.provider.ts
+++ src/providers/cluster.provider.ts
@@ -1,6 +1,9 @@
 import { Injectable } from "@nestjs/common";
 import * as cluster from "cluster";
-import * as os from "os"
+import * as os from "os";
+import * as http from "http";
+import { WEB_SERVER_PORT, WEB_SERVER_HOST } from "@env/config";
+import { setupMaster } from "@socket.io/sticky";
 
 const numCPUs = os.cpus().length;
 
@@ -9,6 +12,12 @@ export class ClusterProvider {
   static clusterize(callback: Function): void {
     if (cluster.isMaster && process.env.NODE_ENV === "production") {
       console.log(`MASTER SERVER (${process.pid}) IS RUNNING `);
+
+      const httpServer = http.createServer();
+      setupMaster(httpServer, {
+        loadBalancingMethod: "least-connection", // either "random", "round-robin" or "least-connection"
+      });
+      httpServer.listen(Number(WEB_SERVER_PORT), WEB_SERVER_HOST);
 
       for (let i = 0; i < numCPUs; i++) {
         cluster.fork();
